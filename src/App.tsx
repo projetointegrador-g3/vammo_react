ed3f937ef6473664f078483231a8265340ddfca1
ZarathosFreya <dessa.neves99@gmail.com>
2025-03-05 12:17:01 -0300

    :tada: Implementação da Segurança

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,22 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/home/Home'
 import About from './pages/about/About'
 import Initial from './pages/initial/Initial'
+import ListarUsuarios from './components/usuarios/listarusuarios/ListarUsuarios'
+import { AuthProvider } from './contexts/AuthContext'
 
 function App() {
   
   return (
     <>
+      <AuthProvider>
       <ToastContainer />
-
       <Routes>
         <Route path="/" element={<Initial />} />
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/all" element={<ListarUsuarios/>}/>
       </Routes>
+      </AuthProvider>
     </>
   )
 }
