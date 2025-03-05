e9f861389b2e1b796a7f7ddaa62343e10e9c7d07
Emily <emilys@genstudents.org>
2025-03-05 18:19:33 -0300

    Updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-<<<<<<< HEAD
 import tailwindcss from '@tailwindcss/vite'
-=======
-import tailwindcss from "@tailwindcss/vite"
->>>>>>> origin/01_home
 
 // https://vite.dev/config/
 export default defineConfig({
