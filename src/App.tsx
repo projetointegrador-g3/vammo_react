4fe3471ef07d57c017a17da017350e296fa3f053
Beatriz <beatriz.rodrigues04@outlook.com>
2025-03-05 17:25:53 -0300

    :sparkles: Estilização da navbar e sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import { ToastContainer } from 'react-toastify'
-import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/home/Home'
 import About from './pages/about/About'
 import Initial from './pages/initial/Initial'
+import Navbar from './components/navbar/Navbar' 
+import Sidebar from './components/sidebar/Sidebar'
 
 function App() {
   
   return (
     <>
       <ToastContainer />
-
+      <Navbar/>
+      <Sidebar/>
       <Routes>
         <Route path="/" element={<Initial />} />
         <Route path="/home" element={<Home />} />
--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,9 +1,24 @@
+import { Car, Gear, House, MapTrifold } from "@phosphor-icons/react"
+import { Link } from "react-router-dom"
 
 const Sidebar = () => {
+
+  if (location.pathname==="/home" || location.pathname==="/perfil" || location.pathname==="/configuracoes" || location.pathname==="/veiculo")
   return (
-    <div>
+    <aside className="flex flex-col flex flex-col fixed flex flex-col items-center top-0 fixed pl-4 bg-black gap-10">
+      <img src="https://ik.imagekit.io/grupo03/Vammo/VAMMO%20(1)%201.png?updatedAt=1741183646285" alt="Logo branco da Vammo!" className="w-25 mr-4 mt-3"></img>
       
-    </div>
+      <nav className="flex flex-col mt-8 mr-5 items-center pb-50 gap-8">
+      <Link to="/home"><House size={24} className="cursor-pointer text-white"/></Link>
+      <Link to="/mapa"><MapTrifold size={24} className="text-white"/></Link>
+      <Link to="/veiculo"><Car size={24} className="text-white"/></Link>
+      <Link to="/configuracoes"><Gear size={24} className="cursor-pointer text-white mt-40"/></Link>
+      </nav>
+    </aside>
+  )
+
+  return(
+    <></>
   )
 }
 
