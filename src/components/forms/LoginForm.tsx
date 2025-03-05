9492534a50c263a12ccfdf9990b9d705c72f9b1c
Emily <emilys@genstudents.org>
2025-03-05 10:51:52 -0300

    :white_check_mark: Adicionando um teste

---
+++ src/components/forms/LoginForm.tsx
@@ -0,0 +1,78 @@
+import { Link } from "react-router-dom";
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
+import { Button } from "../ui/button";
+import { cn } from "../../utils/cn";
+
+export function LoginForm({
+    className,
+    ...props
+  }: React.ComponentPropsWithoutRef<'form'>) {
+  
+    return (
+      <form className={cn('flex flex-col gap-6', className)} {...props}>
+        <div className='flex flex-col items-center gap-2 text-center'>
+          <h1 className='text-2xl font-bold'>Boas-vindas!</h1>
+          <p className='text-balance text-sm'>
+            Entre com seu email e senha para ter acesso
+          </p>
+        </div>
+  
+        {/* Inicio Forms */}
+        <div className='grid gap-6'>
+          <div className='grid gap-2'>
+            <Label htmlFor='email'>Email</Label>
+            <Input id='email' type='email' placeholder='admin@email.com' required />
+          </div>
+  
+          {/* Senha */}
+          <div className='grid gap-2'>
+            <div className='flex items-center'>
+              <Label htmlFor='password'>Senha</Label>
+              <a
+                href='#'
+                className='ml-auto text-sm underline-offset-4 hover:underline'>
+                {/* Esqueceu sua senha? */}
+              </a>
+            </div>
+            <Input id='password' type='password' placeholder='********' required />
+          </div>
+
+          {/* Lembrar de mim */}
+          <div className='flex items-center gap-2 text-sm'>
+            <input type='checkbox' id='lembrar' 
+            className='border-2 border-orange-600 rounded p-2'/>
+            <label htmlFor='lembrar'>Lembrar-me</label>
+          
+
+          {/* Esqueceu a senha */}
+            <Link to='/forgot-password' className='ml-15 underline-offset-4 hover:underline'>
+              Esqueceu a senha?
+            </Link>
+          </div>
+
+
+          {/* Botões */}
+          <Button type='submit' className='w-full'>Entrar</Button>
+
+          {/* Barra horizontal */}
+          <div className='relative text-center text-sm '>
+            <span className='relative z-10 px-2 text-muted-foreground'>
+              Ou entre com
+            </span>
+
+            {/* Botão Google */}
+            <img src='https://ik.imagekit.io/grupo03/DishDash/google-sigh-up.png?updatedAt=1740670098307'
+            className='w-10 mt-3 mx-35'/>
+          </div>
+        </div>
+
+        <div className='text-center text-sm'>
+          Ainda não tem uma conta?{' '}
+          <Link to='/register' className='text-orange-700'>
+            Cadastre-se!
+          </Link>
+        </div>
+      </form>
+    )
+  }
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,10 +1,36 @@
+import { NavLink } from "react-router-dom"
+import { LoginForm } from "../../components/forms/LoginForm"
 
 
 const Login = () => {
   return (
-    <div>
-      
+    
+    <div className='grid min-h-svh lg:grid-cols-2'>
+      <div className='flex flex-col gap-4 p-6 md:p-5'>
+        <div className='flex justify-center gap-2 md:justify-start'>
+          <NavLink to='/' className='flex items-center gap-2 font-medium'>
+            <div className='flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground'>
+            </div>
+            <img 
+            src='https://ik.imagekit.io/grupo03/DishDash/DishDash-logo.png?updatedAt=1740667879038'
+            className='w-25' />
+          </NavLink>
+        </div>
+
+      <div className='flex flex-1 items-center justify-center'>
+        <div className='w-full max-w-xs'>
+          <LoginForm />
+        </div>
+      </div>
     </div>
+    <div className='relative hidden bg-muted lg:block'>
+      <img
+        src='https://ik.imagekit.io/grupo03/DishDash/Copy?updatedAt=1740668460868'
+        alt='Imagem'
+        className='h-[100vh] w-[100vh] mx-15'
+      />
+    </div>
+  </div> 
   )
 }
 
